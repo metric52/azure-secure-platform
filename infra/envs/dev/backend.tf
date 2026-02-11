7bb56f80e563868e0c7476b9f6643af6893ff69b
metric52 <78958354+metric52@users.noreply.github.com>
2026-02-11 12:08:52 +0000

    Update backend.tf
    Adding remote state info for dev

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatestephen01"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
