e2b2498ce6371c9b081c87cb66f23d57903adef9
metric52 <78958354+metric52@users.noreply.github.com>
2026-02-11 12:39:41 +0000

    Update main.tf
    adding a resouce group to dev

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-asp-dev"
+  location = var.location
+
+  tags = {
+    environment = "dev"
+    project     = "azure-secure-platform"
+    managed_by  = "terraform"
+    owner       = "stephen"
+  }
+}
